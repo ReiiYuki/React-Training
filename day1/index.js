af027fb402d90210844946aed2d35bedea617064
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-10 02:18:32 +0700

    Finish Tutorial Day1

--- day1/index.js
+++ day1/index.js
@@ -1,11 +1,17 @@
-// index.js
+
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+
+// import สไตล์เข้ามาในชื่อ styles
+import styles from './styles.scss'
 
 export default class HelloWorld extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        {/* ให้ข้อความของเราประยุกต์สไตล์ของคลาส greeting แบบเฉพาะจุด */}
+        <h1 className={styles.greeting}>Hello World</h1>
+      </div>
     )
   }
 }
--- day1/webpack.config.js
+++ day1/webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   // เปิดใช้งาน sourcemap ด้วยโหมด eval
@@ -15,5 +16,56 @@ module.exports = {
 
     // หลังจากรวมร่างทุกไฟล์เข้าเป็นไฟล์เดียวแล้ว ให้ไฟล์เดียวนั้นชื่ออะไร
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        // ใช้ Regular Expression ทดสอบ ถ้าไฟล์ไหนลงท้ายด้วย js หรือ jsx
+        // ให้ใช้ babel-loader
+        test: /\.(js|jsx)$/,
+
+        // ไม่รวม node_modules เนื่องจากเป็นของที่คนอื่นเขียน
+        // เราไม่ต้องใส่ใจ
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      },
+      {
+        // สำหรับไฟล์นามสกุล css ให้ใช้ Loader สองตัวคือ css-loader และ style-loader
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        // ใช้ Loader สามตัวสำหรับ scss
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              sourceMap: true,
+              module: true,
+              localIdentName: '[local]___[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'sass-loader',
+            query: {
+              outputStyle: 'expanded',
+              sourceMap: true
+            }
+          },
+          'postcss-loader' // เพิ่ม postcss
+        ]
+      }
+    ],
+  },
+  postcss: function () {
+    return [autoprefixer]; // สั่งให้ autoprefix ให้เรา
   }
 };
