f601ffbdd198b8b09b78eb8534e84fe1d35b76ef
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-09 10:52:36 +0700

    install react react-dom webpack

---
+++ day1/index.js
@@ -0,0 +1,13 @@
+// index.js
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+render(<HelloWorld />, document.getElementById('app'))
